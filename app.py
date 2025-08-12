2dba3c42fbe29df2553c6688ee528bb8bdabfeb5
harry8527 <harjeet3singhmehta@gmail.com>
2025-08-12 23:46:09 +0530

    Made a change to show error message in red color if the prompt length is less than 4. Also remove the js logic to show alert, as it was not working

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ def process(input_prompt):
     translation_obj = TranslateUserPrompt()
     img_obj = GenerateImage()
     if len(input_prompt.split()) < 4:
-        return "Please enter a prompt which has atleast 4 words.", None
+        return '<span style="color:red;">Please enter a prompt which has atleast 4 words.</span>', None
     eng_prompt = translation_obj.translate(text=input_prompt)    
     image = img_obj.generate_image(prompt=eng_prompt)
     return eng_prompt, image
 
 
 # iface = gr.Interface(fn = process,inputs=gr.Textbox(label="Enter your prompt<br>The prompt can be in Hindi, English, Spanish, or Punjabi."), outputs=[gr.Textbox("Output Image"), gr.Image()], title="Multilingual Text-to-Image Generator")
-
 with gr.Blocks() as demo:
     gr.Markdown("# Multi-lingual Text-to-Image generator.\nSupports Hindi, English, Spanish, or Punjabi.")
     with gr.Row():
@@ -25,16 +24,7 @@ with gr.Blocks() as demo:
     submit_btn.click(
         fn=process,
         inputs=input_prompt,
-        outputs=[translated_prompt, output_image],
-        js="""
-        (translated, image) => {
-            if(translated.startsWith("Please enter a prompt which")){
-                alert(translated);
-                return [];
-            }
-            return [translated, image]
-        }
-        """
+        outputs=[translated_prompt, output_image]
     )
     # Logic to show pop-up when the input prompt length is less than 4.
 
